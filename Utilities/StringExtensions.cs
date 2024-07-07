8b8f5d6e650094e5ff64526d12c261d61fb794a9
soo-bak <znyous@gmail.com>
2024-07-07 17:18:44 +0900

    :alembic: Commit for Testing CI System (#12)

--- Utilities/StringExtensions.cs
+++ Utilities/StringExtensions.cs
@@ -1,11 +1,17 @@
 using System;
 
-public static class StringExtensions {
-  public static string Reverse(this string str) {
-    if (string.IsNullOrEmpty(str)) return str;
+namespace SUL {
 
-    char[] charArr = str.ToCharArray();
-    Array.Reverse(charArr);
-    return new string(charArr);
+  public static class StringExtensions {
+    public static string Reverse(this string str) {
+      if (string.IsNullOrEmpty(str))
+        return str;
+
+      char[] charArr = str.ToCharArray();
+      Array.Reverse(charArr);
+
+      return new string(charArr);
+    }
   }
+
 }

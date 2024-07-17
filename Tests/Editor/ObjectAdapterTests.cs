950f1ca77f76277f0d0fc8d44f622ba99f24bed0
soo-bak <znyous@gmail.com>
2024-07-18 02:13:53 +0900

    :wrench: Add dll references at the '.asmdef' files.

---
+++ Tests/Editor/ObjectAdapterTests.cs
@@ -0,0 +1,15 @@
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SUL_Tests.Adapters {
+
+  [TestFixture]
+  [Category("Adapters")]
+  public class ObjectAdapterTests {
+    
+
+  }
+}

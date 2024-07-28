6179b878a9ecfb85f213202f6a3f249f816ba4ea
soo-bak <znyous@gmail.com>
2024-07-28 20:48:18 +0900

    :alembic: Test for CI System (#36)

--- .github/scripts/combine-results.js
+++ .github/scripts/combine-results.js
@@ -34,51 +34,58 @@ function combineResults(playmode, editmode) {
   return {
     Generatedon: [new Date().toISOString()],
     Author: ['soo-bak'],
-    Assemblies: [parseInt(playmode.Assemblies[0]) + parseInt(editmode.Assemblies[0])],
-    Classes: [parseInt(playmode.Classes[0]) + parseInt(editmode.Classes[0])],
-    Files: [parseInt(playmode.Files[0]) + parseInt(editmode.Files[0])],
+    Assemblies: [parseInt(playmode.Assemblies[0])],
+    Classes: [parseInt(playmode.Classes[0])],
+    Files: [parseInt(playmode.Files[0])],
     Coveredlines: [parseInt(playmode.Coveredlines[0]) + parseInt(editmode.Coveredlines[0])],
-    Uncoveredlines: [parseInt(playmode.Uncoveredlines[0]) + parseInt(editmode.Uncoveredlines[0])],
-    Coverablelines: [parseInt(playmode.Coverablelines[0]) + parseInt(editmode.Coverablelines[0])],
-    Totallines: [parseInt(playmode.Totallines[0]) + parseInt(editmode.Totallines[0])],
+    Uncoveredlines: [(parseInt(playmode.Uncoveredlines[0]) - parseInt(editmode.Coveredlines[0])) +
+                     (parseInt(editmode.Uncoveredlines[0]) - parseInt(playmode.Coveredlines[0]))],
+    Coverablelines: [parseInt(playmode.Coverablelines[0])],
+    Totallines: [parseInt(playmode.Totallines[0])],
     Coveredmethods: [parseInt(playmode.Coveredmethods[0]) + parseInt(editmode.Coveredmethods[0])],
-    Totalmethods: [parseInt(playmode.Totalmethods[0]) + parseInt(editmode.Totalmethods[0])],
+    Totalmethods: [parseInt(playmode.Totalmethods[0])],
   };
 }
 
-function createCommentBody(summary) {
+function createCommentBody(playmodeResults, editmodeResults, summary) {
+  const playModeLineCoverage = (playmodeResults.Coveredlines[0] / playmodeResults.Coverablelines[0] * 100).toFixed(1);
+  const EditModeLineCoverage = (editmodeResults.Coveredlines[0] / editmodeResults.Coverablelines[0] * 100).toFixed(1);
+
+  const playModeMethodCoverage = (playmodeResults.Coveredmethods[0] / playmodeResults.Totalmethods[0] * 100).toFixed(1);
+  const editModeMethodCoverage = (editmodeResults.Coveredmethods[0] / editmodeResults.Totalmethods[0] * 100).toFixed(1);
+
   const lineCoverage = (summary.Coveredlines[0] / summary.Coverablelines[0] * 100).toFixed(1);
   const methodCoverage = (summary.Coveredmethods[0] / summary.Totalmethods[0] * 100).toFixed(1);
 
   return `
   ### 🍉 Combined Code Coverage Summary 🧐
 
-  | Metric               | Value                        |
-  |----------------------|------------------------------|
-  | Generated on         | ${summary.Generatedon[0]}    |
-  | Author               | ${summary.Author[0]}         |
-  | Assemblies           | ${summary.Assemblies[0]}     |
-  | Classes              | ${summary.Classes[0]}        |
-  | Files                | ${summary.Files[0]}          |
-  | Covered Lines        | **${summary.Coveredlines[0]}**   |
-  | Uncovered Lines      | ${summary.Uncoveredlines[0]} |
-  | Coverable Lines      | ${summary.Coverablelines[0]} |
-  | Total Lines          | **${summary.Totallines[0]}**     |
-  | Line Coverage        | ${formatCoverage(lineCoverage)}     |
-  | Covered Methods      | **${summary.Coveredmethods[0]}** |
-  | Total Methods        | **${summary.Totalmethods[0]}**   |
-  | Method Coverage      | ${formatCoverage(methodCoverage)}   |
+  | Info                 | Value                        | Mode     |
+  |----------------------|------------------------------|----------|
+  | Generated on         | ${summary.Generatedon[0]}    | -        |
+  | Author               | ${summary.Author[0]}         | -        |
+  | Assemblies           | ${summary.Assemblies[0]}     | -        |
+  | Classes              | ${summary.Classes[0]}        | -        |
+  | Files                | ${summary.Files[0]}          | -        |
+  | Covered Lines        | **${summary.Coveredlines[0]}**   | **Play : ${playmodeResults.Coveredlines[0]}**<br> **Edit : ${editmodeResults.Coveredlines[0]}** |
+  | Uncovered Lines      | ${summary.Uncoveredlines[0]} | Play : ${playmodeResults.Uncoveredlines[0]}<br> Edit: ${editmodeResults.Uncoveredlines[0]} |
+  | Coverable Lines      | ${summary.Coverablelines[0]} | Play : ${playmodeResults.Coverablelines[0]}<br> Edit : ${editmodeResults.Coverablelines[0]} |
+  | Total Lines          | **${summary.Totallines[0]}**     | -        |
+  | Line Coverage        | ${formatCoverage(lineCoverage)}     | Play : ${formatCoverage(playModeLineCoverage)}<br> Edit : ${formatCoverage(EditModeLineCoverage)} |
+  | Covered Methods      | **${summary.Coveredmethods[0]}** | Play : ${playmodeResults.Coveredmethods[0]}<br> Edit : ${editmodeResults.Coveredmethods[0]} |
+  | Total Methods        | **${summary.Totalmethods[0]}**   | -        |
+  | Method Coverage      | ${formatCoverage(methodCoverage)}   | Play : ${formatCoverage(playModeMethodCoverage)}<br> Edit : ${formatCoverage(editModeMethodCoverage)} |
   `;
 }
 
 function formatCoverage(coverage) {
   const coverageNum = parseFloat(coverage);
   if (coverageNum === 100) {
-    return `$\\{\\rm{\\color{#00FF00}100\\%}}$`;
+    return `$\\{\\\\rm{\\color{#00FF00}100\\\\%}}$`;
   } else if (coverageNum >= 80) {
-    return `$\\{\\rm{\\color{#FFD700}${coverageNum}\\%}}$`;
+    return `$\\{\\\\rm{\\color{#FFD700}${coverageNum}\\\\%}}$`;
   } else {
-    return `$\\{\\rm{\\color{#8B0000}${coverageNum}\\%}}$`;
+    return `$\\{\\\\rm{\\color{#8B0000}${coverageNum}\\\\%}}$`;
   }
 }
 

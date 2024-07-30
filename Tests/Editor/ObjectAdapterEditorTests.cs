94a8e746f213a901c583c9cabd4a7a529be8e3ec
soo-bak <znyous@gmail.com>
2024-07-31 00:21:24 +0900

    :construction: Work in progress (#38)

--- Tests/Editor/ObjectAdapterEditorTests.cs
+++ Tests/Editor/ObjectAdapterEditorTests.cs
@@ -1,8 +1,12 @@
 using NSubstitute;
 using NUnit.Framework;
 using SUL.Adapters;
+using System.Collections;
 using System.Linq;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
 
 namespace SUL_Tests.Adapters {
 
@@ -20,12 +24,26 @@ public class ObjectAdapterEditorTests {
 
   [TearDown]
   public void TearDown() {
-    if (testObj != null)
-      Object.Destroy(testObj);
+    if (testObj != null) Object.Destroy(testObj);
     testObj = null;
 
-    adapter?.DestroyImmediate(adapter);
+    if (adapter != null) Object.DestroyImmediate(adapter as Object);
     adapter = null;
+
+    var curScene = SceneManager.GetActiveScene();
+    EditorSceneManager.CloseScene(curScene, true);
+  }
+
+  [Test]
+  public void GetInstanceID_ReturnsCorrectID() {
+    // Arrange
+    var expectedID = testObj.GetInstanceID();
+
+    // Act
+    var result = adapter.GetInstanceID();
+
+    // Assert
+    Assert.AreEqual(expectedID, result);
   }
 
   [Test]
@@ -74,6 +92,80 @@ public class ObjectAdapterEditorTests {
   }
 
   [Test]
+  public void FindObjectOfType_GenericMethod_ReturnsCorrectObject() {
+    // Arrange
+    var testCmp = new GameObject().AddComponent<TestComponent>();
+
+    // Act
+    var foundCmp = adapter.FindObjectOfType<TestComponent>();
+
+    // Assert
+    Assert.IsNotNull(foundCmp);
+    Assert.AreEqual(testCmp, foundCmp);
+
+    // Cleanup
+    Object.DestroyImmediate(testCmp.gameObject);
+  }
+
+  [Test]
+  public void FindObjectOfType_WithType_ReturnsCorrectObject() {
+    // Arrange
+    var testCmp = new GameObject().AddComponent<TestComponent>();
+
+    // Act
+    var foundObj = adapter.FindObjectOfType(typeof(TestComponent));
+
+    // Assert
+    Assert.IsFalse(foundObj == null);
+    Assert.IsInstanceOf<IObjectAdapter>(foundObj);
+    Assert.AreEqual(testCmp.name, foundObj.Name);
+
+    // Cleanup
+    Object.DestroyImmediate(testCmp.gameObject);
+  }
+
+  [Test]
+  public void FindObjectsOfType_GenericMethod_ReturnsCorrectObjects() {
+    // Arrange
+    var testCmp1 = new GameObject().AddComponent<TestComponent>();
+    var testCmp2 = new GameObject().AddComponent<TestComponent>();
+
+    // Act
+    var results = adapter.FindObjectsOfType<TestComponent>();
+
+    // Assert
+    Assert.IsNotNull(results);
+    Assert.AreEqual(2, results.Length);
+    Assert.IsTrue(results.Contains(testCmp1));
+    Assert.IsTrue(results.Contains(testCmp2));
+
+    // Cleanup
+    Object.DestroyImmediate(testCmp1.gameObject);
+    Object.DestroyImmediate(testCmp2.gameObject);
+  }
+
+  [Test]
+  public void FindObjectsOfType_WithType_ReturnsCorrectObjects() {
+    // Arrange
+    var testCmp1 = new GameObject().AddComponent<TestComponent>();
+    var testCmp2 = new GameObject().AddComponent<TestComponent>();
+
+    // Act
+    var results = adapter.FindObjectsOfType(typeof(TestComponent));
+
+    // Assert
+    Assert.IsNotNull(results);
+    Assert.AreEqual(2, results.Length);
+    Assert.IsTrue(results.All(r => r is IObjectAdapter));
+    Assert.IsTrue(results.Any(r => r.Name == testCmp1.name));
+    Assert.IsTrue(results.Any(r => r.Name == testCmp2.name));
+
+    // Cleanup
+    Object.DestroyImmediate(testCmp1.gameObject);
+    Object.DestroyImmediate(testCmp2.gameObject);
+  }
+
+  [Test]
   public void Instantiate_CreatesNewObject() {
     // Arrange
     var mockAdapter = Substitute.For<IObjectAdapter>();
@@ -118,8 +210,8 @@ public class ObjectAdapterEditorTests {
 
   [Test]
   public void Instantiate_WithPosition_CreatesNewObjectAtPosition() {
-      // Arrange
-      var mockOriginal = Substitute.For<IObjectAdapter>();
+    // Arrange
+    var mockOriginal = Substitute.For<IObjectAdapter>();
     var mockInstantiated = Substitute.For<IObjectAdapter>();
     var position = new Vector3(1, 2, 3);
     var rotation = Quaternion.Euler(30, 60, 90);
@@ -146,6 +238,8 @@ public class ObjectAdapterEditorTests {
     Assert.IsNotNull(result);
     Assert.IsInstanceOf<IObjectAdapter>(result);
   }
+
+  private class TestComponent : MonoBehaviour { }
 }
 
 } // namespace
--- Tests/Runtime/ObjectAdapterPlayTests.cs
+++ Tests/Runtime/ObjectAdapterPlayTests.cs
@@ -4,12 +4,16 @@ using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 using SUL.Adapters;
 using System.Collections;
+using System.Linq;
 
 namespace SUL_Tests.Adapters {
 
 [TestFixture]
 [Category("Adapters")]
 public class ObjectAdapterPlayTests {
+  const string INACTIVE_GOBJ_NAME = "Inactive";
+  const string ACTIVE_GOBJ_NAME = "Active";
+
   private IObjectAdapter adapter = null;
   private Object testObj = null;
 
@@ -23,11 +27,10 @@ public class ObjectAdapterPlayTests {
 
   [UnityTearDown]
   public IEnumerator TearDown() {
-    if (testObj != null)
-      Object.Destroy(testObj);
+    if (testObj != null) Object.Destroy(testObj);
     testObj = null;
 
-    adapter?.Destroy(adapter);
+    if (adapter != null) Object.Destroy(adapter as Object);
     adapter = null;
 
     yield return null;
@@ -57,10 +60,109 @@ public class ObjectAdapterPlayTests {
     // Assert
     Assert.IsNotNull(adapter);
     Assert.IsFalse(adapter == null);
-
   }
 
-  private IEnumerator SimulateSceneTransition() {
+  [UnityTest]
+  public IEnumerator FindObjectOfType_GenericMethod_IncludeInactive_ReturnsCorrectObject() {
+    // Arrange
+    var inactiveObj = new GameObject(INACTIVE_GOBJ_NAME);
+    inactiveObj.SetActive(false);
+
+    var testCmp = inactiveObj.AddComponent<TestComponent>();
+
+    // Act
+    var foundCmp = adapter.FindObjectOfType<TestComponent>(true);
+
+    // Assert
+    Assert.IsNotNull(foundCmp);
+    Assert.AreEqual(testCmp, foundCmp);
+
+    // Cleanup
+    Object.Destroy(inactiveObj);
+    yield return null;
+  }
+
+  [UnityTest]
+  public IEnumerator FindObjectOfType_WithType_IncludeInactive_ReturnsCorrectObject()
+  {
+    // Arrange
+    var inactiveObj = new GameObject(INACTIVE_GOBJ_NAME);
+    inactiveObj.SetActive(false);
+
+    inactiveObj.AddComponent<TestComponent>();
+
+    // Act
+    var foundObj = adapter.FindObjectOfType(typeof(TestComponent), true);
+    yield return null;
+
+    // Assert
+    Assert.IsNotNull(foundObj);
+    Assert.IsInstanceOf<IObjectAdapter>(foundObj);
+    Assert.AreEqual(inactiveObj, foundObj);
+
+    // Cleanup
+    Object.Destroy(inactiveObj);
+    yield return null;
+  }
+
+  [UnityTest]
+  public IEnumerator FindObjectsOfType_GenericMethod_IncludeInactive_ReturnsCorrectObjects()
+  {
+    // Arrange
+    var activeObj = new GameObject(ACTIVE_GOBJ_NAME);
+    var inactiveObj = new GameObject(INACTIVE_GOBJ_NAME);
+    inactiveObj.SetActive(false);
+
+    var testCmp_1 = activeObj.AddComponent<TestComponent>();
+    var testCmp_2 = inactiveObj.AddComponent<TestComponent>();
+
+    // Act
+    var foundCmps = adapter.FindObjectsOfType<TestComponent>(true);
+    yield return null;
+
+    // Assert
+    Assert.IsNotNull(foundCmps);
+    Assert.AreEqual(2, foundCmps.Length);
+    Assert.IsTrue(foundCmps.Contains(testCmp_1));
+    Assert.IsTrue(foundCmps.Contains(testCmp_2));
+
+    // Cleanup
+    Object.Destroy(activeObj);
+    Object.Destroy(inactiveObj);
+    yield return null;
+  }
+
+  [UnityTest]
+  public IEnumerator FindObjectsOfType_WithType_IncludeInactive_ReturnsCorrectObjects()
+  {
+    // Arrange
+    var activeObj = new GameObject(ACTIVE_GOBJ_NAME);
+    var inactiveObj = new GameObject(INACTIVE_GOBJ_NAME);
+    inactiveObj.SetActive(false);
+
+    var testCmp_1 = activeObj.AddComponent<TestComponent>();
+    var testCmp_2 = inactiveObj.AddComponent<TestComponent>();
+
+    // Act
+    var foundObjs = adapter.FindObjectsOfType(typeof(TestComponent), true);
+    yield return null;
+
+    // Assert
+    Assert.IsNotNull(foundObjs);
+    Assert.AreEqual(2, foundObjs.Length);
+    Assert.IsTrue(foundObjs.All(r => r is IObjectAdapter));
+    Assert.IsTrue(foundObjs.Any(r => r.Name == testCmp_1.name));
+    Assert.IsTrue(foundObjs.Any(r => r.Name == testCmp_2.name));
+
+    // Cleanup
+    Object.Destroy(activeObj);
+    Object.Destroy(inactiveObj);
+    yield return null;
+  }
+
+  private class TestComponent : MonoBehaviour { }
+
+    private IEnumerator SimulateSceneTransition() {
     var originScene = SceneManager.GetActiveScene();
 
     var tempSceneName = "TempScene";
@@ -73,5 +175,3 @@ public class ObjectAdapterPlayTests {
     yield return SceneManager.SetActiveScene(originScene);
   }
 }
-
-}// namesapce

139d52464a51f8de0ea58b6c5e023e10eeddeeb3
soo-bak <znyous@gmail.com>
2024-08-11 00:07:52 +0900

    :pencil2: Correct Typo (#44)

--- Tests/Utilities/StringExtensionTests.cs
+++ Tests/Utilities/StringExtensionTests.cs
@@ -1,38 +1,36 @@
 using NUnit.Framework;
-using SUL;
 
-// XXX Consider namespace for classification structure in terms of hierarchy.
-namespace SUL_Tests.Utilities {
+namespace SUL.Utilities {
 
 [TestFixture]
-  [Category("StringManipulation")]
-  public class StringExtensionTests {
+[Category("StringManipulation")]
+public class StringExtensionTests {
 
-    [Test]
-    public void Reverse_WithNonEmptyString_ReturnsReversedString() {
-      // Arrange
-      var original = "soobak";
-      var expected = "kaboos";
+  [Test]
+  public void Reverse_WithNonEmptyString_ReturnsReversedString() {
+    // Arrange
+    var original = "soobak";
+    var expected = "kaboos";
 
-      // Act
-      string result = original.Reverse();
-      
-      // Assert
-      Assert.AreEqual(expected, result);
-    }
+    // Act
+    string result = original.Reverse();
 
-    [Test]
-    public void Reverse_WithEmptyString_ReturnsEmptyString() {
-      // Arrange
-      var original = string.Empty;
-      var expected = string.Empty;
+    // Assert
+    Assert.AreEqual(expected, result);
+  }
 
-      // Act
-      string result = original.Reverse();
+  [Test]
+  public void Reverse_WithEmptyString_ReturnsEmptyString() {
+    // Arrange
+    var original = string.Empty;
+    var expected = string.Empty;
 
-      // Assert
-      Assert.AreEqual(expected, result);
-    }
+    // Act
+    string result = original.Reverse();
 
+    // Assert
+    Assert.AreEqual(expected, result);
   }
 }
+
+} // namespace
